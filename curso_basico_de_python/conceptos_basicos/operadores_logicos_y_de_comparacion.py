4fdd0b9c9b8caf4399a1a9637922ca49ac286481
LaureanoVera <laureanoigvera@gmail.com>
2022-01-03 18:03:20 -0300

    curso basico de python: operadores logicos y de comparacion

---
+++ curso_basico_de_python/conceptos_basicos/operadores_logicos_y_de_comparacion.py
@@ -0,0 +1,22 @@
+# LOGICOS
+estudiante = True
+trabaja = True
+japones = False
+
+print(estudiante and trabaja)
+print(estudiante and japones)
+print(trabaja or japones)
+print(not (estudiante))
+
+# COMPARACION
+print("COMPARACION:")
+a = 1
+b = 3
+c = 2
+
+print(a == c)
+print(a != b)
+print(b < a)
+print(c > b)
+print(5 <= 5)
+print(12 >= 5)
