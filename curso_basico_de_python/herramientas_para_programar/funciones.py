b126bd3ad41067e380ab6c0475c4928c1b9c64da
LaureanoVera <laureanoigvera@gmail.com>
2022-01-04 06:41:04 -0300

    curso basico de python: funciones

---
+++ curso_basico_de_python/herramientas_para_programar/funciones.py
@@ -0,0 +1,17 @@
+def print_message(msg):
+    print(msg)
+
+
+print_message("Laureano")
+
+
+def conversacion(msg):
+    print("Hello")
+    print("Hi, how are you?")
+    print("Pretty good")
+    print(msg)
+
+
+conversacion("End...")
+conversacion("End!")
+conversacion("Bye :[")
