cc7ca3729f7b8e98eb0bc78ac9cf0576f744fb78
LaureanoVera <laureanoigvera@gmail.com>
2022-01-04 22:05:31 -0300

    curso basico de python: trabajando con texto: cadena de caracteres

---
+++ curso_basico_de_python/herramientas_para_programar/trabajando_con_texto.py
@@ -0,0 +1,10 @@
+name = "  Lauriano"
+name = name.strip()  # Clean Emply Space
+# name = name.upper() # All Letters Upper
+# name = name.capitalize() # First Letter Upper
+# name = name.lower() # All Letters Lower
+name = name.replace("i", "e")
+# name = name[0]
+name = len(name)
+
+print(name)
